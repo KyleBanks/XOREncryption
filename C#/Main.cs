279a6d5c6dd456e6bdfa158ab3d8c48ddf51113a
KyleBanks <kyle@nthgensoftware.com>
2013-10-06 20:15:32 -0400

    Added C# implementation

---
+++ C#/Main.cs
@@ -0,0 +1,24 @@
+using System;
+
+class XOREncryption
+{
+	private static string encryptDecrypt(string input) {
+		char[] key = {'K', 'C', 'Q'};
+		char[] output = new char[input.Length];
+				
+		for(int i = 0; i < input.Length; i++) {
+			output[i] = (char) (input[i] ^ key[i % key.Length]);
+		}
+		
+		return new string(output);
+	}
+	
+	public static void Main (string[] args)
+	{
+		string encrypted = encryptDecrypt("kylewbanks.com");
+		Console.WriteLine ("Encrypted:" + encrypted);
+		
+		string decrypted = encryptDecrypt(encrypted);
+		Console.WriteLine ("Decrypted:" + decrypted);
+	}
+}
