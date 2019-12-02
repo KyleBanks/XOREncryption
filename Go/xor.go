235975bd23783f9e6a35ed331b5e942c4e263960
nexus166 <nexus166@users.noreply.github.com>
2019-12-02 01:11:45 +0100

    call len on key just once

--- Go/xor.go
+++ Go/xor.go
@@ -3,9 +3,9 @@ package xor
 // EncryptDecrypt runs a XOR encryption on the input string, encrypting it if it hasn't already been,
 // and decrypting it if it has, using the key provided.
 func EncryptDecrypt(input, key string) (output string) {
+	kL := len(key)
 	for i := range input {
-		output += string(input[i] ^ key[i%len(key)])
+		output += string(input[i] ^ key[i%kL])
 	}
-
 	return output
 }
